3bbd42b718233e112da79a69e2208c8b6ce752e9
Ivan Gusev <i.gusev@infocom.local>
2020-10-10 23:33:13 +0300

    Сверстал блок cover.

--- webpack.config.js
+++ webpack.config.js
@@ -23,7 +23,7 @@ module.exports = {
     {
       test: /\.(png|jpg|gif|ico|svg)$/,
       use: [
-        'file-loader?name=../images/[name].[ext]',
+        'file-loader?name=../images/[name].[ext]&esModule=false',
         {
           loader: 'image-webpack-loader',
           options: {}
